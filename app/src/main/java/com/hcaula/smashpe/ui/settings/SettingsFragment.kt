05292885119f7fb06bced69ec0c253f88681b8f1
Henrique Caúla <henrique.caula3@gmail.com>
2020-11-29 17:59:17 -0300

    feat(Settings): add option to reset API Key

--- app/src/main/java/com/hcaula/smashpe/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/hcaula/smashpe/ui/settings/SettingsFragment.kt
@@ -1,31 +1,55 @@
 package com.hcaula.smashpe.ui.settings
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.hcaula.smashpe.R
+import kotlinx.android.synthetic.main.settings_fragment.view.*
 
-class DashboardFragment : Fragment() {
-
-    private lateinit var settingsViewModel: SettingsViewModel
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        settingsViewModel =
-            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.settings_fragment, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        root.settings_reset_api_key.setOnClickListener(onResetKey)
+
         return root
+    }
+
+    private val onResetKey = View.OnClickListener {
+        val builder = AlertDialog.Builder(context, R.style.DialogTheme)
+        builder
+            .setTitle(R.string.dialog_title)
+            .setMessage(
+                this.getString(
+                    R.string.reset_api_key_dialog
+                )
+            )
+            .setPositiveButton(
+                R.string.dialog_confirm
+            ) { _, _ ->
+                val sharedPreferences =
+                    context?.getSharedPreferences("Settings", Context.MODE_PRIVATE)
+
+                with(sharedPreferences?.edit()) {
+                    this?.putString("API_KEY", "")
+                    this?.apply()
+                }
+
+                activity?.finish()
+            }
+            .setNegativeButton(
+                R.string.dialog_deny
+            ) { _, _ -> }
+
+        builder.create().show()
     }
 }
